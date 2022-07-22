aa86088c24991f37b1b6322a2113249e07fd668e
RawScriptContainer <58143044+RawScriptContainer@users.noreply.github.com>
2022-07-22 20:20:54 +0200

    Update IMGUI-UI-Library.lua

--- IMGUI-UI-Library.lua
+++ IMGUI-UI-Library.lua
@@ -17,6 +17,7 @@ do
 end
 
 local imgui = Instance.new("ScreenGui")
+syn.protect_gui(imgui)
 local Prefabs = Instance.new("Frame")
 local Label = Instance.new("TextLabel")
 local Window = Instance.new("ImageLabel")
